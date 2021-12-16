bc0cb33b998ec10d1f1cb34c086325fdc8c7fa5b
Motasem Aldiab <mdiab@atypon.com>
2021-12-15 18:53:26 -0800

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main");
+    }
+
+    public static int cubeX(int x){
+        return x * x *x;
+    }
+}
---
+++ src/test/java/TestCases.java
@@ -0,0 +1,8 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+public class TestCases {
+    @Test
+    public void test1(){
+        assertEquals(27, Main.cubeX(3));
+    }
+}
