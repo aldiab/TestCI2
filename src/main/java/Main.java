dd3f06c8cbed85f0fa3867f6cf18eb3b22475c45
aldiab <motasem.aldiab@gmail.com>
2021-12-16 18:31:14 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Main");
+        System.out.println(cubeX(5));
     }
 
     public static int cubeX(int x){
