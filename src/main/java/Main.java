05153268df5c8a55e052ed5a18fa88d0fd976955
Motasem Aldiab <mdiab@atypon.com>
2021-12-16 20:05:08 -0800

    modifying Testcases

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Main");
         System.out.println(cubeX(5));
+        System.out.println(squareX(5));
     }
 
     public static int cubeX(int x){
