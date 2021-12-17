6e07bd43c1783f2e925e269be6f2eec507e1e641
aldiab <motasem.aldiab@gmail.com>
2021-12-17 10:07:12 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println(cubeX(5));
         System.out.println(squareX(5));
         System.out.println("feature");
+        System.out.println("Testing with class");
     }
 
     public static int cubeX(int x){
