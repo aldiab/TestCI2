66ce58ff50869de12ef41b8e20936962a5fabdbd
aldiab <motasem.aldiab@gmail.com>
2022-09-23 00:15:23 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println(squareX(5));
         System.out.println("feature");
         System.out.println("Testing with class");
-        System.out.println("Another test");
+        
 
         System.out.println("hello world!");
 
