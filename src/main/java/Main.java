0beea5503d1b683560e8d163f6fb03e908f83171
Motasem Aldiab <mdiab@atypon.com>
2023-01-27 09:35:09 -0800

    modifying repo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,7 @@ public class Main {
         System.out.println("Main");
         System.out.println(cubeX(5));
         System.out.println(squareX(5));
-        System.out.println("feature");
-        System.out.println("Testing with class");
-        System.out.println("Another test");
 
-        System.out.println("hello world!");
-
-        System.out.println("Adding some code from the client side");
-
-        System.out.println("Adding a line from Intelli");
 
 
         
