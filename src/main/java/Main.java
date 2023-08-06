176b65871e51a95055d9a0cb2a3168ceca2500d7
Panidou <105602883+Panidou@users.noreply.github.com>
2023-08-06 19:11:22 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,7 @@ public class Main {
         System.out.println(cubeX(5));
         System.out.println(squareX(5));
         System.out.println("A new modification from IntelliJ");
-
-
+        System.out.println("A new modification from Anna");
     }
    
     public static int cubeX(int x){
