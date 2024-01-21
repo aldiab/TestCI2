02c510d9319edf79bf2ef8ea90952f1b00756d11
Omar <80687619+omars7z@users.noreply.github.com>
2024-01-21 20:06:59 +0300

    Omar's commit Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Main");
         System.out.println(cubeX(5));
         System.out.println(squareX(5));
-        System.out.println("Ruwaid");
+        System.out.println("Omar");
 
     }
    
