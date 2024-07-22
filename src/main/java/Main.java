c43c7bdbb85e9b8573e53100d8662a7ceeedd294
Motasem Aldiab <mdiab@atypon.com>
2024-07-23 01:32:53 +0300

    testing IntelliJIDEA

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println(cubeX(5));
         System.out.println(squareX(5));
         System.out.println("Omar");
+        System.out.println("Omar");
 
     }
    
