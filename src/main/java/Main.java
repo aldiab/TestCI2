4c09a7fef71710aed4771a19a2098fd7b22e6ab4
Motasem Aldiab <mdiab@atypon.com>
2024-07-23 19:20:14 +0300

    testing IntelliJIDEA

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("Motasem");
         System.out.println("Motasem");
         System.out.println("Rukny");
+        System.out.println("Another line");
     }
    
     public static int cubeX(int x){
