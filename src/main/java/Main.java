b29b8b72af105b6f2419bb866354bce82ae88efa
Motasem Aldiab <mdiab@atypon.com>
2025-02-23 10:48:01 -0800

    A new build

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,12 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Main");
         System.out.println(cubeX(5));
         System.out.println(squareX(5));
-        System.out.println("Omar");
-        System.out.println("Motasem");
-        System.out.println("Motasem");
-        System.out.println("Rukny");
-        System.out.println("Another line");
-        System.out.println("Another line2");
-        System.out.println("Another line3");
-        System.out.println("Another line3");
+        System.out.println("A new build");
+
     }
    
     public static int cubeX(int x){
